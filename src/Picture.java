764d889a7748622dd624dc9a1f9535b79b4262c7
Derek Harrison <hderek18@comcast.net>
2018-03-06 10:47:37 -0700

    Complete Exercises 3-6

--- src/Picture.java
+++ src/Picture.java
@@ -95,6 +95,51 @@ public class Picture extends SimplePicture {
 		}
 	}
 
+	public void keepOnlyBlue(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setRed(0);
+				pixelObj.setGreen(0);
+			}
+		}
+	}
+
+	public void negate() {
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setRed(255 - pixelObj.getRed());
+				pixelObj.setGreen(255 - pixelObj.getGreen());
+				pixelObj.setBlue(255 - pixelObj.getBlue());
+			}
+		}
+	}
+
+	public void grayscale() {
+		int avg = 0;
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray: pixels) {
+			for (Pixel pixelObj : rowArray) {
+				avg = (pixelObj.getRed() + pixelObj.getBlue() + pixelObj.getGreen()) / 3;
+				pixelObj.setRed(avg);
+				pixelObj.setGreen(avg);
+				pixelObj.setBlue(avg);
+			}
+		}
+	}
+
+	public void fixUnderwater() {
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray: pixels) {
+			for (Pixel pixelObj : rowArray) {
+				if (pixelObj.getRed() < 21 && pixelObj.getBlue() > 155) {
+					pixelObj.setGreen(0);
+				}
+			}
+		}
+	}
+
 	/**
 	 * Method that mirrors the picture around a vertical mirror in the center of
 	 * the picture from left to right
